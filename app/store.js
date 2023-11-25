c9600ca6170c9eaf5ee2e40b165386dd0fe79faf
Amin Mirzapour <aminmrzpr@gmail.com>
2023-11-25 18:03:12 +0330

    async toolkit

--- app/store.js
+++ app/store.js
@@ -1,8 +1,18 @@
 const configureStore = require('@reduxjs/toolkit').configureStore;
-const cakeReducer = require('../features/cake/cakeSlice.js');
+// const reduxLogger = require('redux-logger');
+const cakeReducer = require('../features/cake/cakeSlice');
+const icecreamReducer = require('../features/icecream/iceCreamSlice');
+const userReducer = require('../features/user/userSlice');
+
+// const logger = reduxLogger.createLogger();
 
 const store = configureStore({
-  reducer: cakeReducer,
+  reducer: {
+    cake: cakeReducer,
+    icecream: icecreamReducer,
+    user: userReducer,
+  },
+  //   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
 
 module.exports = store;
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
-const store = require('./app/store.js');
+const store = require('./app/store');
 const cakeActions = require('./features/cake/cakeSlice').cakeActions;
+const icecreamActions =
+  require('./features/icecream/iceCreamSlice').icecreamActions;
+const fetchUsers = require('./features/user/userSlice').fetchUsers;
 
 console.log('initial State', store.getState());
 const unsubscribe = store.subscribe(() => {
-  console.log('Updated Store', store.getState());
+  console.log('State Update', store.getState());
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.reStocked(3));
+store.dispatch(fetchUsers());
 
-unsubscribe();
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.reStocked(3));
+
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.reStocked(2));
+
+// unsubscribe();
