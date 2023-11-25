9b0d22c73469fcce28cccd2d2c26f42750d14bd2
Amin Mirzapour <aminmrzpr@gmail.com>
2023-11-25 11:11:40 +0330

    first commit

---
+++ features/cake/cakeSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+const cakeSlice = createSlice({
+  name: 'cake',
+  initialState,
+  reducers: {
+    ordered: (state) => {
+      state.numOfCakes--;
+    },
+    reStocked: (state, action) => {
+      state.numOfCakes = +action.payload;
+    },
+  },
+});
+
+module.exports = cakeSlice.reducer;
+module.exports.cakeActions = cakeSlice.actions;
